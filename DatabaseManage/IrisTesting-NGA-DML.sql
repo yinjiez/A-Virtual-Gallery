7dff4c3bf53ef19fc9b1fb3a14af179ae61ab55c
Iris Ma <irisma@seas.upenn.edu>
2022-03-30 21:06:55 -0400

    Yes I want to commit this

--- DatabaseManage/IrisTesting-NGA-DML.sql
+++ DatabaseManage/IrisTesting-NGA-DML.sql
@@ -476,3 +476,7 @@ GROUP BY table_schema;
 SELECT table_schema AS "Database", SUM(data_length + index_length) / 1024 / 1024 / 1024 AS "Size (GB)"
 FROM information_schema.TABLES
 GROUP BY table_schema;
+
+
+
+# demo for github
