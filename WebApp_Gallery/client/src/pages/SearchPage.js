1479fe89478d2cfc8522410ee878e2103ec7d6bf
lingzix <71335808+lingzix@users.noreply.github.com>
2022-04-16 11:59:27 -0700

    Major update: added waterfall display, completed search functions

--- WebApp_Gallery/client/src/pages/SearchPage.js
+++ WebApp_Gallery/client/src/pages/SearchPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {FormInput, FormGroup, Button, Badge, CardBody, CardTitle, Progress } from "shards-react";
-
+import {FormInput, FormGroup, Button, Badge, CardBody, CardTitle, Container, Progress } from "shards-react";
+import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
 import {
     Table,
     Row,
@@ -13,11 +13,11 @@ import {
     Layout,
     Input,
     Form,
-    Card
+    Card,
+    List
 
 } from 'antd'
 
-import { getArtwork, getSimilarArtworks } from '../fetcher'
 import { getSearchByFilter, getSearchByKeyword, getNaughtyByHeight, getNaughtyByBirthYear } from '../fetcher'
 import MenuBar from '../components/MenuBar';
 
@@ -30,6 +30,7 @@ const { Title } = Typography;
 const { Column, ColumnGroup } = Table;
 const { Meta } = Card;
 
+
 class SearchPage extends React.Component {
     constructor(props) {
         super(props)
@@ -39,61 +40,115 @@ class SearchPage extends React.Component {
         //window.location.search: ?objectID=${objectID}
         //substring(1): objectID=${objectID}
         //after split: ["objectID", objectID]; index 1 gets the object ID
-            searchArtwork:'',
-            searchArtist:'',
+            searchArtwork: '',
+            searchArtist: '',
             filterClassification: 'painting',
-            filterNationality: 'American',
-            filterBeginyear: 1700,
-            filterEndyear: 1800,
+            filterNationality: '',
+            filterBeginyear: 0,
+            filterEndyear: 2022,
             filterStyle: '',
-            selectedObjectID: window.location.search ? window.location.search.substring(1).split('=')[1] : 0,
-            searchResults: []
+            searchResults:[],
+            visible: 6,
+            mode:'k'
 
         }
 
         this.goToArtwork = this.goToArtwork.bind(this)
         this.searchArtwork = this.searchArtwork.bind(this)
         this.searchArtist = this.searchArtist.bind(this)
-        // this.filterClassification = this.filterClassification.bind(this)
-        // this.filterNationality = this.filterNationality.bind(this)
-        // this.filterBeginyear = this.filterBeginyear.bind(this)
-        // this.filterEndyear = this.filterEndyear.bind(this)
-        // this.filterStyle = this.filterStyle.bind(this)
+        this.handleClassChange = this.handleClassChange.bind(this)
+        this.handleNationalityChange = this.handleNationalityChange.bind(this)
+        this.handleBegYearChange = this.handleBegYearChange.bind(this)
+        this.handleEndYearChange = this.handleEndYearChange.bind(this)
+        this.handleStyleChange = this.handleStyleChange.bind(this)
+        this.loadMore = this.loadMore.bind(this)
+        this.updateSearchResults = this.updateSearchResults.bind(this)
+        this.updateFilterResults = this.updateFilterResults.bind(this)        
+
     }
 
     goToArtwork(objectID) {
         window.location = `/artwork?objectID=${objectID}`
     }
     
+    /** handle search box changes */
     searchArtwork(event) {
       this.setState({ searchArtwork: event.target.value })
     }
 
     searchArtist(event) {
-      this.setState({ searchArtist: event.target.value })
+        this.setState({ searchArtist: event.target.value })
     }
 
-    updateSearchResults() {
-      getSearchByKeyword(this.state.searchArtwork, this.state.searchArtist, null, null).then(res => {
-          this.setState({ searchResults: res.results })
-      })
-  }
+    /** handle filter changes */
+    handleClassChange(event) {
+        this.setState({ filterClassification: event.key })
+    }
 
-   updateFilterResults(){
-    getSearchByFilter(this.state.filterNationality, this.state.filterStyle, this.state.filterBeginyear, this.state.filterEndyear, this.state.filterClassification, null, null).then(res => {
-      this.setState({ searchResults: res.results })
-  })
-   }
+    handleNationalityChange(event) {
+      this.setState({ filterNationality: event.target.value })
+    }
+
+    handleStyleChange(event) {
+        this.setState({ filterStyle: event.target.value })
+    }
+
+    handleBegYearChange(event) {
+        this.setState({ filterBeginyear: event.target.value })
+    }
+
+    handleEndYearChange(event) {
+        this.setState({ filterEndyear: event.target.value })
+    }
+
+    loadMore() {
+      this.setState( {visible: this.state.visible + 6})
+    }
+  
+
+
+    /**update keyword search */
+    updateSearchResults() {
+      getSearchByKeyword(this.state.searchArtwork, this.state.searchArtist, 1, this.state.visible).then(res => {
+        var jsonObj ={}
+        var list2 = []
+        for (let i = 0; i < res.results.length; i++) {
+            jsonObj = res.results[i]
+            list2.push(jsonObj)
+            this.setState({ searchResults: list2})
+        }
+        this.setState( {mode: 'k'})
+      })
+    }
+
+    /** update filter search */
+    updateFilterResults(){
+        getSearchByFilter(this.state.filterNationality, this.state.filterStyle, this.state.filterBeginyear, this.state.filterEndyear, this.state.filterClassification, 1, this.state.visible).then(res => {
+          var jsonObj ={}
+          var list4 = []
+          for (let i = 0; i < res.results.length; i++) {
+              jsonObj = res.results[i]
+              list4.push(jsonObj)
+              this.setState({ searchResults: list4})
+          }
+          this.setState( {mode: 'f'})
+        })
+    }
+
 
     componentDidMount() {
-      getSearchByKeyword(this.state.searchArtwork, this.state.searchArtist, null, null).then(res => {
-        this.setState({ searchResults: res.results })
-    })
 
-      getSearchByFilter(this.state.filterNationality, this.state.filterStyle, this.state.filterBeginyear, this.state.filterEndyear, this.state.filterClassification, null, null).then(res => {
-      this.setState({ searchResults: res.results })
-    })
-  }
+        getSearchByKeyword(this.state.searchArtwork, this.state.searchArtist, 1, this.state.visible).then(res => {
+            var jsonObj ={}
+            var list1 = []
+            for (let i = 0; i < res.results.length; i++) {
+                jsonObj = res.results[i]
+                list1.push(jsonObj)
+                this.setState({ searchResults: list1})
+          }
+        })
+          
+    }
 
   render() {
     return (
@@ -108,7 +163,7 @@ class SearchPage extends React.Component {
             <Row>
               <Col flex={2}><FormGroup style={{ width: '20vw', margin: '0 auto' }}>
                 <label>Search by artwork:</label>
-                <FormInput placeholder="artwork" OnChange={this.state.searchArtwork} />
+                <FormInput placeholder="artwork" value={this.state.searchArtwork} onChange={this.searchArtwork} />
               </FormGroup></Col>
               <Col flex={2}><FormGroup style={{ width: '10vw' }}>
                 <Button theme="dark" style={{ marginTop: '3vh' }} onClick={this.updateSearchResults}>Search</Button>
@@ -116,161 +171,57 @@ class SearchPage extends React.Component {
 
               <Col flex={2}><FormGroup style={{ width: '20vw', margin: '0 auto' }}>
                 <label>Search by artist:</label>
-                <FormInput placeholder="artist" OnChange={this.state.searchArttist} />
+                <FormInput placeholder="artist" value={this.state.searchArtist} onChange={this.searchArtist} />
               </FormGroup></Col>
               <Col flex={2}><FormGroup style={{ width: '10vw' }}>
                 <Button theme="dark" style={{ marginTop: '3vh' }} onClick={this.updateSearchResults}>Search</Button>
               </FormGroup></Col>
             </Row>
+            <Row>
+            </Row>
           </Form>
         </Row>
-        
         <Layout className="site-layout-background" style={{ padding: '24px 0'}}>
           <Sider className="site-layout-background" width = {300} style={{marginLeft: '1.5vw'}} >
             <Menu
               mode="inline"
-              defaultSelectedKeys={['1']}
-              defaultOpenKeys={['sub1']}
+              defaultSelectedKeys={['painting']}
               style={{ height: '100%' }}
+              onSelect={this.handleClassChange}
             >
                <Button theme="dark" style={{ marginTop: '1vh'}} size='30' block> Filter:</Button>
                <Row style={{ padding: '0 24px', minHeight: 10 }}></Row>
-              <SubMenu key="classification" title="Classification">
-                <Menu.Item key='painting' onClick={this.state.filterClassification}>Painting</Menu.Item>
-                <Menu.Item key='print'  onClick={this.state.filterClassification}>Print</Menu.Item>
-                <Menu.Item key='drawing'  onClick={this.state.filterClassification}>Drawing</Menu.Item>
+              <SubMenu key="classification" title="Classification" >
+                <Menu.Item key='painting' >Painting</Menu.Item>
+                <Menu.Item key='print'  >Print</Menu.Item>
+                <Menu.Item key='drawing' >Drawing</Menu.Item>
               </SubMenu>
               <Row style={{ padding: '0 24px', minHeight: 10 }}></Row>
               <Form name="filter" labelCol={{ span: 8 }} wrapperCol={{ span: 12 }} >
-                <Form.Item name="style" label="Style" > <Input placeholder="style" OnChange={this.state.filterStyle}/> </Form.Item>
-                <Form.Item name="nationality" label="Nationality" > <Input placeholder="American" OnChange={this.state.filterNationality}/> </Form.Item>
-                <Form.Item name="beginYear" label="Begin Year" > <Input placeholder={1700} OnChange={this.state.filterBeginyear}/> </Form.Item>
-                <Form.Item name="endYear" label="End Year" > <Input placeholder={1800} OnChange={this.state.filterEndyear}/> </Form.Item>
+                <Form.Item name="style" label="Style" > <Input placeholder="style" onChange={this.handleStyleChange}/> </Form.Item>
+                <Form.Item name="nationality" label="Nationality" > <Input placeholder="American" onChange={this.handleNationalityChange}/> </Form.Item>
+                <Form.Item name="beginYear" label="Begin Year" > <Input type="text" pattern="[0-9]*" placeholder={1500}  onChange={this.handleBegYearChange}/> </Form.Item>
+                <Form.Item name="endYear" label="End Year" > <Input type="text" pattern="[0-9]*" placeholder={1800} onChange={this.handleEndYearChange}/> </Form.Item>
               </Form>
               <Button theme="dark" style={{ marginTop: '2vh', marginBotton: '2vh' }} onClick={this.updateFilterResults} block>Search</Button>
             </Menu>
           </Sider>
-          <Content style={{ padding: '0 24px', minHeight: 280 }}>
-            <Row gutter={16} justify="space-around" align="middle">
-              <Col span={7}>
-                <Card hoverable = {true}
-                  style={{ width: 220 }}
-                  cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />} onClick={() => this.goToArtwork(this.state.selectedObjectID)}
-                >
-                  <Meta title="Europe Street beat"/>
-                </Card>
-              </Col>
-              <Col span={7}>
-                <Card hoverable = {true}
-                  style={{ width: 220}}
-                  cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />} onClick={() => this.goToArtwork(this.state.selectedObjectID)}
-                >
-                  <Meta title="Europe Street beat"/>
-                </Card>
-              </Col>
-              <Col span={7}>
-                <Card hoverable = {true}
-                  style={{ width: 220}}
-                  cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />} onClick={() => this.goToArtwork(this.state.selectedObjectID)}
-                >
-                  <Meta title="Europe Street beat"/>
-                </Card>
-              </Col>
-            </Row>
-            <Row style={{ padding: '0 24px', minHeight: 30 }}></Row>
-            <Row gutter={16} justify="space-around" align="middle">
-              <Col span={7}>
-                <Card hoverable = {true}
-                  style={{ width: 220 }}
-                  cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />} onClick={() => this.goToArtwork(this.state.selectedObjectID)}
-                >
-                  <Meta title="Europe Street beat"/>
-                </Card>
-              </Col>
-              <Col span={7}>
-                <Card hoverable = {true}
-                  style={{ width: 220}}
-                  cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />} onClick={() => this.goToArtwork(this.state.selectedObjectID)}
-                >
-                  <Meta title="Europe Street beat"/>
-                </Card>
-              </Col>
-              <Col span={7}>
-                <Card hoverable = {true}
-                  style={{ width: 220}}
-                  cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />} onClick={() => this.goToArtwork(this.state.selectedObjectID)}
-                >
-                  <Meta title="Europe Street beat"/>
-                </Card>
-              </Col>
-            </Row>
+          <Content style={{ padding: '0 80px'}}>
+            <Masonry columnsCount={3}>
+              {this.state.searchResults.map(item => <Card hoverable = {true}
+              style={{ width: 220, marginTop: '2vh',whiteSpace: 'normal' }}
+              cover={<img src={item.thumbURL} maxHeight={300}></img>} onClick={() => this.goToArtwork(item.objectID)}>
+                  <Meta title={<a>{item.title}</a>}></Meta>
+                  <Meta description={item.attribution}></Meta>
+            </Card>)}
+            </Masonry>
+            <Row justify='center'><button          
+            onClick={() => [this.state.mode ==='k'? [this.loadMore(), this.updateSearchResults()] :  [this.loadMore(), this.updateFilterResults()]]} type="button" className="load-more">Load more</button></Row>
           </Content>
         </Layout>
         </Divider>
-        
-
-        {/* Naughty search */}
-        <Divider>
-        <Row style={{ padding: '0 24px', minHeight: 30 }}></Row>
-        <Row justify="space-around" align="middle"> 
-        <Title level ={2}>Not Sure What To See? Try Explore Collections From Here</Title>
-          {/* create search fields */}
-          <Form style={{ width: '80vw', margin: '0 auto', marginTop: '2vh', marginBottom: '2vh' }}>
-            <Row>
-              <Col flex={2}><FormGroup style={{ width: '20vw', margin: '0 auto' }}>
-                <label>Enter your height (in centimeters):</label>
-                <FormInput placeholder="height (eg. 180)" OnChange={this.state.searchArtwork} />
-              </FormGroup></Col>
-              <Col flex={2}><FormGroup style={{ width: '10vw' }}>
-                <Button theme="dark" style={{ marginTop: '3vh' }} onClick={this.updateSearchResults}>Search</Button>
-              </FormGroup></Col>
-
-              <Col flex={2}><FormGroup style={{ width: '20vw', margin: '0 auto' }}>
-                <label>Enter your birth year:</label>
-                <FormInput placeholder="year (eg. 2000)" OnChange={this.state.searchArttist} />
-              </FormGroup></Col>
-              <Col flex={2}><FormGroup style={{ width: '10vw' }}>
-                <Button theme="dark" style={{ marginTop: '3vh' }} onClick={this.updateSearchResults}>Search</Button>
-              </FormGroup></Col>
-            </Row>
-          </Form>
-        </Row>
-        <Layout className="site-layout-background" style={{ padding: '24px 0'}}>
-          <Content style={{ padding: '0 24px', minHeight: 280 }}>
-            <Row gutter={16} justify="space-around" align="middle" >
-              <Col style={{ padding: '0 24px', width: 10 }} />
-              <Col span={7}>
-                <Card hoverable={true}
-                  style={{ width: 220 }}
-                  cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />} onClick={() => this.goToArtwork(this.state.selectedObjectID)}
-                >
-                  <Meta title="Europe Street beat" />
-                </Card>
-              </Col>
-              <Col span={7}>
-                <Card hoverable={true}
-                  style={{ width: 220 }}
-                  cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />} onClick={() => this.goToArtwork(this.state.selectedObjectID)}
-                >
-                  <Meta title="Europe Street beat" />
-                </Card>
-              </Col>
-              <Col span={7}>
-                <Card hoverable={true}
-                  style={{ width: 220 }}
-                  cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />} onClick={() => this.goToArtwork(this.state.selectedObjectID)}
-                >
-                  <Meta title="Europe Street beat" />
-                </Card>
-              </Col>
-            </Row>
-            
-          </Content>
-          </Layout>
-        </Divider>
       </div>
       
-
     )
   }
 }
