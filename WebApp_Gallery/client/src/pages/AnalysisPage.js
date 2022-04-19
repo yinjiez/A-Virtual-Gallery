04bd3aff22004250a8cf2df1acc15071f1244678
lingzix <71335808+lingzix@users.noreply.github.com>
2022-04-18 19:19:21 -0700

    AnalysisPage: Completed Portraits-across-time

--- WebApp_Gallery/client/src/pages/AnalysisPage.js
+++ WebApp_Gallery/client/src/pages/AnalysisPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { FormInput, FormGroup, Button, Badge, CardBody, CardTitle, Progress, CardImgOverlay } from "shards-react";
+import { FormInput, FormGroup, Badge, CardBody, CardTitle, Progress, CardImgOverlay } from "shards-react";
 import { WordCloud, Heatmap } from '@ant-design/plots';
 
 import {
@@ -13,7 +13,9 @@ import {
     Layout,
     Input,
     Form,
-    Card
+    Card,
+    Button,
+    Tooltip
 
 } from 'antd'
 
@@ -21,10 +23,11 @@ import ItemsCarousel from 'react-items-carousel';
 import {Link, Redirect} from 'react-router-dom';
 
 import { getArtwork, getSimilarArtworks } from '../fetcher'
-import { getSearchByFilter, getSearchByKeyword, getNaughtyByHeight, getNaughtyByBirthYear } from '../fetcher'
+import { getAnalysisOverview, getAnalysisByType, getPortraitsAcrossTime } from '../fetcher'
+
 import MenuBar from '../components/MenuBar';
 
-import { UserOutlined, LaptopOutlined, NotificationOutlined, LeftOutlined, RightOutlined } from '@ant-design/icons';
+import { UserOutlined, LaptopOutlined, NotificationOutlined, LeftOutlined, RightOutlined, RedoOutlined } from '@ant-design/icons';
 
 const { Header, Content, Sider, Footer } = Layout;
 
@@ -67,6 +70,7 @@ const DemoWordCloud = () => {
   };  
 
 
+
 //   Pre-setup for heatmap
 const DemoHeatmap = () => {
     const [data, setData] = useState([]);
@@ -102,7 +106,7 @@ const DemoHeatmap = () => {
     return <Heatmap {...config} />;
   };
 
-//horizonal scroll
+//horizonal scroll implementation
 
 const StyleCollection = () => {
     const [activeItemIndex, setActiveItemIndex] = useState(0);
@@ -157,10 +161,9 @@ const StyleCollection = () => {
             <p style={{color: 'white', margin: 0}}>284 Items</p>
         </div></Link></a>
         <a><Link to={`/search/byFilter?style=abstract expression`}><div style={{backgroundImage: "linear-gradient(to bottom, rgba(256, 256, 256, 0.2), rgba(0, 0, 0, 0.2)), url('https://api.nga.gov/iiif/aefb55ba-3f17-410f-9c42-f7fd797320f5/full/!600,600/0/default.jpg')",
-        width: 300, height: 350, backgroundSize: 'cover', backgroundPosition: 'top', padding: '20px', top: 0}}>
+        width: 300, height: 350, backgroundSize: '130%', backgroundPosition: 'top', padding: '20px', top: 0}}>
             <Typography.Title level={4} style={{color: 'white', margin: 0}}>Abstract Expressionist</Typography.Title>
-            <p style={{color: 'white', margin: 0}}>624 Items</p>
-
+            <p style={{color: 'white', margin: 0}}>283 Items</p>
         </div></Link></a>
         </ItemsCarousel>
       </div>
@@ -168,29 +171,66 @@ const StyleCollection = () => {
   };
 
 
+
 class AnalysisPage extends React.Component {
     constructor(props) {
         super(props)
 
         this.state = {
-
+            portraitSet: [], //the set of 6 periods currently being redered
+            p1: [], //6 portraits from 15th century (1400 -1499)
+            p2: [], //6 portraits from 16th century (1500 -1599)
+            p3: [], //6 portraits from 17th century (1600 -1699)
+            p4: [], //6 portraits from 18th century (1700 -1799)
+            p5: [], //6 portraits from 19th century (1800 -1899)
+            p6: [], //6 portraits from 20th century (1900 -1999)
+            k: 0
         }
     
-    this.goToStyleSearch = this.goToStyleSearch.bind(this)
+    this.shufflePortraitSet = this.shufflePortraitSet.bind(this)
+    this.goToArtwork = this.goToArtwork.bind(this)
+
     }
 
 
-
-    goToStyleSearch(style) {
-        window.location = `/search/byFilter=nationality=&style=${style}&beginYear=0&endYear=2022&classfication=painting&page=1&pagesize=60`
-        
+    goToArtwork(objectID) {
+        window.location = `/artwork?objectID=${objectID}`
     }
 
-
-
+    shufflePortraitSet(){
+        if (this.state.k < 7){
+            this.setState({k: this.state.k + 1})
+        } else {
+            this.setState({k: 0})
+        }
+        var temp = []
+        temp.push(this.state.p1[this.state.k])
+        temp.push(this.state.p2[this.state.k])
+        temp.push(this.state.p3[this.state.k])
+        temp.push(this.state.p4[this.state.k])
+        temp.push(this.state.p5[this.state.k])
+        temp.push(this.state.p6[this.state.k])
+        this.setState({ portraitSet: temp})
+    }
 
     componentDidMount() {
 
+        Promise.all([
+            getPortraitsAcrossTime("painting", 1400, 1499, 1, 8),
+            getPortraitsAcrossTime("painting", 1500, 1599, 1, 8),
+            getPortraitsAcrossTime("painting", 1600, 1699, 1, 8),
+            getPortraitsAcrossTime("painting", 1700, 1799, 1, 8),
+            getPortraitsAcrossTime("painting", 1800, 1899, 1, 8),
+            getPortraitsAcrossTime("painting", 1900, 1999, 1, 8)
+        ]).then(([r1, r2, r3, r4, r5, r6]) => {
+            this.setState({ p1: r1.results}) //store 5 portraits from 15th century
+            this.setState({ p2: r2.results}) //store 5 portraits from 16th century
+            this.setState({ p3: r3.results}) //store 5 portraits from 17th century
+            this.setState({ p4: r4.results}) //store 5 portraits from 18th century
+            this.setState({ p5: r5.results}) //store 5 portraits from 19th century
+            this.setState({ p6: r6.results}) //store 5 portraits from 20th century
+            this.setState({ portraitSet: [this.state.p1[0], this.state.p2[0],  this.state.p3[0], this.state.p4[0],this.state.p5[0], this.state.p6[0]]}) //push the 1st item of each period into the display set
+        })
     }
 
     render() {
@@ -214,7 +254,7 @@ class AnalysisPage extends React.Component {
                         </Form> */}
                         {/* Display keyword graph on top */}
                     <Row justify="space-around" align="middle">
-                        <Title level={3}>Collections Keywords</Title>
+                        <Title level={2}>Collections Keywords</Title>
                     </Row>
                     <Form style={{ width: '60vw', margin: '0 auto', minHeight: 280}}>
                         <DemoWordCloud />
@@ -224,63 +264,31 @@ class AnalysisPage extends React.Component {
                 </Divider>
 
                 <Row justify="space-around" align="middle">
-                <Title level={3}>Top 8 Styles in Collection</Title>
+                <Title level={2}>Top 8 Styles in Collection</Title>
                 </Row>
+                {/* Collection by styles, fully set up above*/}
                 <StyleCollection />
                  
+
+                {/* Portraits across time*/}
+                <Row style={{ padding: '0 24px', minHeight: 50 }}></Row>
+                <Row justify="space-around" align="middle">
+                <Title level={2}>Portraits across time</Title>
+                </Row>
+                <div style={{ padding: `0 50px` }}><ItemsCarousel
+                        alwaysShowChevrons={true}
+                        rightChevron={<Tooltip title="Try me!"><Button shape="circle" icon={<RedoOutlined />} onClick={this.shufflePortraitSet} size='large' /></Tooltip>}
+                        numberOfCards={6}
+                        gutter={0}
+                        outsideChevron
+                        chevronWidth={50}>
+                {this.state.portraitSet.map(item => <a><div style={{backgroundImage: `url('${item.thumbURL.replace('!200,200','!500,500')}`,
+                    width: 250, height: 350, backgroundSize: 'cover', padding: '20px'}} onClick={() => this.goToArtwork(item.objectID)}>
+                    <Typography.Title level={4} style={{color: 'white', margin: 0, position: 'absolute'}}>{item.endYear}s</Typography.Title>
+                    <p style={{color: 'white', width: 210, margin: 0, position: 'absolute', bottom: 12, wordBreak: 'normal', whiteSpace: 'normal', overflowWrap: 'break-word'}}>{item.title}</p>
+                    </div></a>)}
+                </ItemsCarousel></div>
                 
-
-                {/* Artworks in different time span*/}
-                {/* <Divider>
-                    <Row style={{ padding: '0 24px', minHeight: 10 }}></Row>
-                    <Layout className="site-layout-background" style={{ padding: '0 50px' }}>
-                        <Content className="site-layout-content" style={{ padding: '0 50px', minHeight: 280 }}>
-                            <Form style={{ width: '80vw', margin: '0 auto', marginTop: '2vh', marginBottom: '2vh' }}>
-                                <Row style={{ padding: '0 24px', minHeight: 10 }}>
-                                    <Row justify="space-around" align="middle">
-                                        <Title level={3}>Artwork Collections In Different Time Span</Title>
-                                    </Row>
-                                    <Row gutter={200} justify="space-around" align="middle">
-                                        <Col span={1}>
-                                            <Card hoverable={true}
-                                                style={{ width: 200 }}
-                                                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />} onClick={() => this.goToArtwork(this.state.selectedObjectID)}
-                                            >
-                                                <Meta title="Europe Street beat" />
-                                            </Card>
-                                        </Col>
-                                        <Col span={1}>
-                                            <Card hoverable={true}
-                                                style={{ width: 200 }}
-                                                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />} onClick={() => this.goToArtwork(this.state.selectedObjectID)}
-                                            >
-                                                <Meta title="Europe Street beat" />
-                                            </Card>
-                                        </Col>
-                                        <Col span={1}>
-                                            <Card hoverable={true}
-                                                style={{ width: 200 }}
-                                                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />} onClick={() => this.goToArtwork(this.state.selectedObjectID)}
-                                            >
-                                                <Meta title="Europe Street beat" />
-                                            </Card>
-                                        </Col>
-                                        <Col span={1}>
-                                            <Card hoverable={true}
-                                                style={{ width: 200 }}
-                                                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />} onClick={() => this.goToArtwork(this.state.selectedObjectID)}
-                                            >
-                                                <Meta title="Europe Street beat" />
-                                            </Card>
-                                        </Col>
-                                    </Row>
-                                    <Row style={{ padding: '0 24px', minHeight: 30 }}></Row>
-                                </Row>
-                            </Form>
-                        </Content>
-                    </Layout>
-                </Divider> */}
-
 
                  {/* Artworks in different region*/}
                  {/* <Divider>
@@ -336,8 +344,9 @@ class AnalysisPage extends React.Component {
                 {/* Display heatmap */}
                 <Divider>
                         {/* Display heatmap graph at bottom */}
+                    <Row style={{ padding: '0 24px', minHeight: 50 }}></Row>
                     <Row justify="space-around" align="middle">
-                        <Title level={3}>Collections Distribution</Title>
+                        <Title level={2}>Collections Distribution</Title>
                     </Row>
                     <Form style={{ width: '100vw', margin: '0 auto', minHeight: 280, marginTop: '5 vh'}}>
                         {/* <DemoHeatmap /> */}
